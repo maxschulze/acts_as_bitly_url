86f112b1357e00fff35f8f557f6b59eed22e8df1
Max  Schulze <maxschulze@Max-Mac-Pro.local>
2010-02-05 20:44:46 +0100

    init

--- init.rb
+++ init.rb
@@ -1,2 +1,4 @@
 require File.dirname(__FILE__) + '/lib/acts_as_bitly_url' 
 ActionView::Base.send(:include, ActsAsBitlyURL)
+ActionController::Base.send(:include, ActsAsBitlyURL)
+ActiveRecord::Base.send(:include, ActsAsBitlyURL)
